0ce018a095c099be9908038130cfe54b0494e60e
EZZAHRAOUI Meriame <meriam.ezzahraoui@gmail.com>
2018-06-02 22:10:21 +0000

    ffff

--- proj/Global.asax.cs
+++ proj/Global.asax.cs
@@ -18,7 +18,6 @@ namespace proj
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            UnityConfig.RegisterComponents();
 
 
         }
