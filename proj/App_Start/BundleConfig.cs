3fcc518ef1d6a82893ef65bdce3e84d226626244
DouiabAsmaa <asmaa.douiab@gmail.com>
2018-06-02 18:43:10 +0200

    LayoutAdmin Ok

--- proj/App_Start/BundleConfig.cs
+++ proj/App_Start/BundleConfig.cs
@@ -26,6 +26,20 @@ namespace proj
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                      "~/Content/plugins/jQuery/jquery-2.2.3.min.js",
+                      "~/Content/bootstrap/js/bootstrap.min.js",
+                      "~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+                      "~/Content/plugins/fastclick/fastclick.js",
+                       "~/Content/dist/js/app.min.js",
+                        "~/Content/dist/js/demo.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Css").Include(
+                      "~/Content/bootstrap/css/bootstrap.min.css",
+                      "~/Content/dist/css/AdminLTE.min.css",
+                      "~/Content/dist/css/skins/_all-skins.min.css"));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
